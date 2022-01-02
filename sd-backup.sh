7f366fb381ce475bf7a44240c601f370cd069056
r-ionescu <96948193+r-ionescu@users.noreply.github.com>
2022-01-02 17:03:13 +0200

    sd-backup.sh v0.7
    restore function added

--- sd-backup.sh
+++ sd-backup.sh
@@ -7,7 +7,7 @@ shopt -s nocasematch
 
 #////////////////////////////////////////////////////////////////////////////////////////////
 
-readonly SCRIPT_VERSION='0.6'
+readonly SCRIPT_VERSION='0.7'
 readonly LINE='==============================================================================='
 declare tmp="${BASH_SOURCE[0]}"
 readonly SCRIPT_PATH="$( cd -- "$(dirname "${tmp}")" >/dev/null 2>&1 ; pwd -P )"
@@ -15,17 +15,22 @@ readonly SCRIPT_NAME="${tmp##*/}"
 readonly SCRIPT_HEADER="\n${LINE}\n ${SCRIPT_NAME} v${SCRIPT_VERSION} - SD card backup for MacOS \n${LINE}\n"
 readonly OS_TYPE="$(uname)"
 
-declare TS _doNotAskForConfirmation _disk disk _imgFile imgFile
-declare -i diskSize
+declare TS CLI_param_action CLI_param_doNotAskForConfirmation _disk disk _imgFile imgFile
+declare -i diskSize imgSize
 
 readonly defaultBS='1m'
 readonly defaultImgFile="./SD-card.bs${defaultBS}.dd.img"
 
-if [[ "$@" =~ -y ]]
+if [[ "$@" =~ "-".*y.* || "$@" =~ "--yes" ]]; then CLI_param_doNotAskForConfirmation="true"; fi
+readonly CLI_param_doNotAskForConfirmation
+
+if [[ "$@" =~ "-".*r.* || "$@" =~ "--restore" ]]
 	then
-		_doNotAskForConfirmation="true"
+		CLI_param_action="RESTORE"
+	else
+		CLI_param_action="BACKUP"
 	fi
-readonly _doNotAskForConfirmation
+readonly CLI_param_action
 
 #////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -34,11 +39,12 @@ function SHOW_USAGE()
 cat <<EOF
 USAGE:
 
-  ${SCRIPT_NAME} [disk-device] [backup-file-name] [-y]
+  ${SCRIPT_NAME} [disk-device] [backup-file-name] [-r | --restore] [-y | --yes]
 
-    disk-device          = disk device (SD card, eg: /dev/disk2).
+    disk-device          = disk device (SD/USB card, eg: /dev/disk2).
     backup-file-name     = backup file name; default value is "${defaultImgFile}"".
-    -y                   = do NOT ask for confirmation for starting backup process.
+		-r | --restore       = if present, restore to disk from backup file.
+    -y | --yes           = do NOT ask for confirmation for starting backup process.
 
 EOF
 
@@ -65,7 +71,6 @@ function searchDisks()
 		for md in "${mountedDisks[@]}"
 		do
 			isMatch=$(diskutil info "$md" 2>/dev/null | grep -oE "${regexPattern}")
-
 			if [ "$isMatch" ]
 				then
 					disk="$md"
@@ -80,7 +85,6 @@ function searchDisks()
 cleanup()
 {
  if [ "${imgFile}" ]; then rm -f "${imgFile}" 2>&1 1>/dev/null; fi
- if [ "${tmpFile}" ]; then rm -f "${tmpFile}" 2>&1 1>/dev/null; fi
 }
 trap cleanup EXIT
 
@@ -108,34 +112,26 @@ if [[ "$@" =~ -{1,2}help ]]
 
 
 disk="$1"
-if [[ "$disk" =~ -y ]]
-  then
-     disk=''
-  fi
+if [[ ! "$@" =~ ^/dev/r{0.1}disk[0-9]+ ]]; then disk=''; fi
+
 if [ ! "$disk" ]
   then
     declare -a mountedDisks=($(diskutil list | grep -oE '\/dev\/disk[0-9]+' | sort -u))
-
 		searchDisks 'SD\s+Card\s+Reader' "${mountedDisks[@]}"
-		searchDisks 'USB\s+SD\s+Reader' "${mountedDisks[@]}"
-
+		searchDisks 'USB\s+SD\s+Reader'  "${mountedDisks[@]}"
 		unset mountedDisks
   fi
 if [ ! "$disk" ]
-then
-  getTimeStamp
-  echo -e "[${TS}] ERROR - Unable to detect SD card automatically.\nPlease specify \"disk-device\" as first parameter of the script.\n"
-  SHOW_USAGE
-  exit 254
-fi
-if [[ ! "$disk" =~ ^/dev/ ]]
-  then
-    disk="/dev/${disk}"
-  fi
+	then
+  	getTimeStamp
+  	echo -e "[${TS}] ERROR - Unable to detect SD card automatically.\nPlease specify \"disk-device\" as first parameter of the script.\n"
+  	SHOW_USAGE
+  	exit 254
+	fi
+
 disk="${disk/\/dev\/disk//dev/rdisk}"
 _disk="${disk/\/dev\/rdisk//dev/disk}"
 readonly _disk disk
-
 
 diskSize=$(diskutil info "$disk" 2>/dev/null | grep -E 'Disk\s+Size' | tr ' ' '\n' | grep -E '\S' | head -5 | tail -1 | grep -oE '[0-9]+')
 readonly diskSize
@@ -149,27 +145,27 @@ if [ ! "$diskSize" ]
 #////////////////////////////////////////////////////////////////////////////////////////////
 
 imgFile="${2:-$defaultImgFile}"
-if [[ "$imgFile" =~ -y ]]
-  then
-     imgFile="${defaultImgFile}"
-  fi
+if [[ "$@" =~ "-".*[ry].* || "$@" =~ "--yes" || "$@" =~ "--restore"  ]]; then imgFile="${defaultImgFile}"; fi
 _imgFile="${imgFile##*/}"
 readonly _imgFile imgFile
 
 #////////////////////////////////////////////////////////////////////////////////////////////
 
-printf '%-20s %s\n' "Disk:" "${disk} (${diskSize} bytes)"
-printf '%-20s %s\n' "Backup file:" "${imgFile}.zip"
+printf '%-30s %s\n'   "Action:"        "${CLI_param_action}"
+printf '%-30s %s\n'   "Disk:"          "${disk} (${diskSize} bytes)"
+printf '%-30s %s\n'   "Backup file:"   "${imgFile}.zip"
 echo
 
 
-if [ ! "${_doNotAskForConfirmation}" ]
+
+if [ ! "${CLI_param_doNotAskForConfirmation}" ]
   then
-    read -p "Start backup process? " -n 1 -r
-    echo -e "\n"
+    read -p "Start ${CLI_param_action} process? " -n 1 -r
+    echo
     if [[ ! $REPLY =~ ^y$ ]]; then exit 1; fi
   fi
 
+sync 2>/dev/null
 
 if [ "$(mount | grep "${_disk}")" ]
   then
@@ -179,33 +175,112 @@ if [ "$(mount | grep "${_disk}")" ]
     echo
   fi
 
-declare -i pid
-rm -f "${imgFile}"     2>&1 1>/dev/null
-rm -f "${imgFile}.zip" 2>&1 1>/dev/null
+ case "${CLI_param_action}" in
+
+	 "backup")
+	 		declare -i pid pidZIP
+			rm -f "${imgFile}"     2>&1 1>/dev/null
+			rm -f "${imgFile}.zip" 2>&1 1>/dev/null
+			getTimeStamp
+			mkfifo "${imgFile}" || exit 251
+
+			if [ "$(pv -V 2>/dev/null)" ]
+				then
+					{
+					 dd if="${disk}" bs="${defaultBS}" | pv  --progress --wait --width 79 --size ${diskSize} | dd of="${imgFile}" bs="${defaultBS}"
+				  } 2>&1 &
+				else
+					dd if="${disk}" of="${imgFile}" bs="${defaultBS}" 2>&1 &
+				fi
+			pid=$!
+
+			if ps -p $pid 2>&1 1>/dev/null
+				then
+					echo "[${TS}] ${CLI_param_action} process started."
+					zip -9q --fifo "${imgFile}.zip" "${imgFile}" &
+					pidZIP=$!
+					while :
+						do
+							if ps -p $pidZIP 2>&1 1>/dev/null
+								then
+									if ! ps -p $pid 2>&1 1>/dev/null
+										then
+											if disown $pidZIP 2>&1 1>/dev/null; then kill -9 $pidZIP 2>&1 1>/dev/null; fi
+											getTimeStamp
+											echo "[${TS}] ${CLI_param_action} ERROR."
+											exit 250
+										fi
+								else
+									break
+								fi
+							sleep 1
+						done
+				else
+					getTimeStamp
+					echo "[${TS}] ${CLI_param_action} ERROR."
+					exit 250
+				fi
+
+			getTimeStamp
+			echo -e "\n[${TS}] Testing ZIP archive."
+			sync 2>/dev/null
+			if ! zip -vT "${imgFile}.zip"; then exit 249; fi
+
+			;;
+
+	 "restore")
+	 		getTimeStamp
+			echo -e "\n[${TS}] Testing ZIP archive."
+	 		sync 2>/dev/null
+	 		if ! zip -vT "${imgFile}.zip"; then exit 249; fi
+
+			imgSize=$(unzip -l "${imgFile}.zip" | tail -1 | grep -oE '^[0-9]+\s+1\s+file' | grep -oE '^[0-9]+')
+			readonly imgSize
+			if [ ! "$imgSize" ]
+			  then
+			    getTimeStamp
+					echo "[${TS}] ERROR - Unsupported archive: ${imgFile}.zip"
+					exit 248
+			  fi
+
+			echo
+			printf '%-30s %s\n'   "Backup image size:"     "${imgSize} bytes"
+			printf '%-30s %s\n'   "Disk size (${disk}):"   "${diskSize} bytes"
+			if [ "${imgSize}" -gt "${diskSize}" ]
+				then
+					getTimeStamp
+					echo "[${TS}] ERROR - Disk size too small."
+					exit 247
+			  fi
+
+			getTimeStamp
+			echo "[${TS}] ${CLI_param_action} process started."
+			if [ "$(pv -V 2>/dev/null)" ]
+				then
+					if ! unzip -p "${imgFile}.zip" | pv  --progress --wait --width 79 --size ${imgSize} | dd of="${disk}" bs="${defaultBS}"
+						then
+							getTimeStamp
+							echo "[${TS}] ${CLI_param_action} ERROR."
+							exit 246
+						fi
+				else
+					if ! unzip -p "${imgFile}.zip" | dd of="${disk}" bs="${defaultBS}"
+					then
+						getTimeStamp
+						echo "[${TS}] ${CLI_param_action} ERROR."
+						exit 246
+					fi
+				fi
+			;;
+
+			*)
+			getTimeStamp
+			echo "[${TS}] ERROR - Invalid operation: ${CLI_param_action}"
+			exit 2
+			;;
+esac
+
 getTimeStamp
-declare tmpFile=$(mktemp -u)
-mkfifo "${imgFile}" || exit 251
-
-if [ "$(pv -V)" ]
-  then
-    ( dd if="${disk}" bs="${defaultBS}" & echo $! >&3 ) 3>"${tmpFile}" | pv  --progress --wait --width 79 --size $diskSize | dd of="${imgFile}" bs="${defaultBS}" &
-		pid=$(<"${tmpFile}")
-  else
-    dd if="${disk}" of="${imgFile}" bs="${defaultBS}" &
-		pid=$!
-  fi
-
-if ps -p $pid 2>&1 1>/dev/null
-	then
-		echo "[${TS}] Backup process started."
-		zip -9q --fifo "${imgFile}.zip" "${imgFile}" || exit 250
-	else
-		exit 250
-	fi
-
-
-
-getTimeStamp
-echo -e "[${TS}] Backup process finished.\n"
+echo -e "\n[${TS}] ${CLI_param_action} process finished.\n"
 sync 2>/dev/null
 exit 0
