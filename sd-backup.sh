f28affa68a68734221d6776b872c03bba4c29ba7
r-ionescu <96948193+r-ionescu@users.noreply.github.com>
2022-01-02 18:46:15 +0200

    sd-backup.sh v0.7.3
    license

--- sd-backup.sh
+++ sd-backup.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
-
+#
+#////////////////////////////////////////////////////////////////////////////////////////////
+#
+#
+#
+# SD card backup for MacOS
+# https://github.com/r-ionescu/SD-card-backup-for-MacOS/blob/main/sd-backup.sh
+# Copyright (C) 2021  Raul Ionescu <raul.ionescu@outlook.com>
+#
+#
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+# or https://raw.githubusercontent.com/r-ionescu/SD-card-backup-for-MacOS/main/LICENSE
+#
+#
+#
+#////////////////////////////////////////////////////////////////////////////////////////////
 
 set -o pipefail
 shopt -s nocasematch
 
-
 #////////////////////////////////////////////////////////////////////////////////////////////
 
-readonly SCRIPT_VERSION='0.7.2'
+readonly SCRIPT_VERSION='0.7.3'
 readonly LINE='==============================================================================='
 declare tmp="${BASH_SOURCE[0]}"
 readonly SCRIPT_PATH="$( cd -- "$(dirname "${tmp}")" >/dev/null 2>&1 ; pwd -P )"
