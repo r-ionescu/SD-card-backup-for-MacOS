5d0dcee16946c358e8c82162d1afe4e8f4d26902
r-ionescu <96948193+r-ionescu@users.noreply.github.com>
2022-01-03 21:53:40 +0200

    sd-backup.sh v0.8
    positional parameters no longer in use

--- sd-backup.sh
+++ sd-backup.sh
@@ -33,7 +33,7 @@ shopt -s nocasematch
 
 #////////////////////////////////////////////////////////////////////////////////////////////
 
-readonly SCRIPT_VERSION='0.7.4'
+readonly SCRIPT_VERSION='0.8'
 readonly LINE='==============================================================================='
 declare tmp="${BASH_SOURCE[0]}"
 readonly SCRIPT_PATH="$( cd -- "$(dirname "${tmp}")" >/dev/null 2>&1 ; pwd -P )"
@@ -41,22 +41,48 @@ readonly SCRIPT_NAME="${tmp##*/}"
 readonly SCRIPT_HEADER="\n${LINE}\n ${SCRIPT_NAME} v${SCRIPT_VERSION} - SD card backup for MacOS \n${LINE}\n"
 readonly OS_TYPE="$(uname)"
 
-declare TS CLI_param_action CLI_param_doNotAskForConfirmation _disk disk _imgFile imgFile
+declare TS _disk disk _imgFile imgFile
 declare -i diskSize imgSize
 
 readonly defaultBS='1m'
 readonly defaultImgFile="./SD-card.bs${defaultBS}.dd.img"
 
+#////////////////////////////////////////////////////////////////////////////////////////////
+#////////////////////////////////////////////////////////////////////////////////////////////
+#////////////////////////////////////////////////////////////////////////////////////////////
+
+declare CLI_param_doNotAskForConfirmation
 if [[ "$@" =~ "-".*y.* || "$@" =~ "--yes" ]]; then CLI_param_doNotAskForConfirmation="true"; fi
 readonly CLI_param_doNotAskForConfirmation
 
+#////////////////////////////////////////////////////////////////////////////////////////////
+
+declare CLI_param_action
 if [[ "$@" =~ "-".*r.* || "$@" =~ "--restore" ]]
 	then
                 CLI_param_action="RESTORE"
 	else
-		CLI_param_action="BACKUP"
+                CLI_param_action="BACKUP"
 	fi
 readonly CLI_param_action
+
+#////////////////////////////////////////////////////////////////////////////////////////////
+
+declare CLI_param_disk
+if [[ "$@" =~ --disk[[:space:]]+([^[:space:]]+)([[:space:]]*|$) ]]
+	then
+		CLI_param_disk="${BASH_REMATCH[1]}"
+	fi
+readonly CLI_param_disk
+
+#////////////////////////////////////////////////////////////////////////////////////////////
+
+declare CLI_param_file
+if [[ "$@" =~ --file[[:space:]]+([^[:space:]]+)([[:space:]]*|$) ]]
+	then
+		CLI_param_file="${BASH_REMATCH[1]}"
+	fi
+readonly CLI_param_file
 
 #////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -65,19 +91,18 @@ function SHOW_USAGE()
 cat <<EOF
 USAGE:
 
-  ${SCRIPT_NAME} [disk-device] [backup-file-name] [-r | --restore] [-y | --yes]
+  ${SCRIPT_NAME} [--disk disk-device] [--file file-name] [-r] [-y]
 
 
-    positional parameters:
 
-      disk-device          SD/USB card, eg:/dev/disk2
-      backup-file-name     backup file name
-                           default value is "${defaultImgFile}""
+      --disk disk-device   SD/USB card, eg: --disk /dev/disk2
 
-
-    optional parameters:
+      --file file-name     backup file name
+                           default value is "${defaultImgFile}"
+                           eg: --file ${defaultImgFile}
 
       -r | --restore       restore to disk from backup file
+
       -y | --yes           do NOT ask for confirmation for starting
 
 EOF
@@ -137,7 +162,7 @@ if [[ ! $OS_TYPE == 'darwin' ]]
 
 
 
-if [[ "$@" =~ -{1,2}help ]]
+if [[ "$@" =~ -{1,2}h(elp)? ]]
 	then
 		SHOW_USAGE
 		exit 0
@@ -145,8 +170,8 @@ if [[ "$@" =~ -{1,2}help ]]
 
 
 
-disk="$1"
-if [[ ! "$@" =~ ^/dev/r{0.1}disk[0-9]+ ]]; then disk=''; fi
+disk="${CLI_param_disk}"
+if [[ ! "${disk}" =~ ^/dev/r{0.1}disk[0-9]+ ]]; then disk=''; fi
 
 if [ ! "$disk" ]
         then
@@ -178,16 +203,15 @@ if [ ! "$diskSize" ]
 
 #////////////////////////////////////////////////////////////////////////////////////////////
 
-imgFile="${2:-$defaultImgFile}"
-if [[ "$@" =~ "-".*[ry].* || "$@" =~ "--yes" || "$@" =~ "--restore"  ]]; then imgFile="${defaultImgFile}"; fi
+imgFile="${CLI_param_file:-$defaultImgFile}"
 _imgFile="${imgFile##*/}"
 readonly _imgFile imgFile
 
 #////////////////////////////////////////////////////////////////////////////////////////////
 
 printf '%-30s %s\n'   "Action:"        "${CLI_param_action}"
-printf '%-30s %s\n'   "Disk:"          "${disk} (${diskSize} bytes)"
 printf '%-30s %s\n'   "Backup file:"   "${imgFile}.zip"
+printf '%-30s %s\n'   "Disk:"          "${disk} (${diskSize} bytes)"
 echo -e "\n"
 
 
@@ -209,108 +233,108 @@ if [ "$(mount | grep "${_disk}")" ]
                 echo
         fi
 
- case "${CLI_param_action}" in
+case "${CLI_param_action}" in
 
-        "backup")
-                declare -i pidDD pidZIP
-                rm -f "${imgFile}"     2>&1 1>/dev/null
-                rm -f "${imgFile}.zip" 2>&1 1>/dev/null
-                getTimeStamp
-                mkfifo "${imgFile}" || exit 251
+       "backup")
+               declare -i pidDD pidZIP
+               rm -f "${imgFile}"     2>&1 1>/dev/null
+               rm -f "${imgFile}.zip" 2>&1 1>/dev/null
+               getTimeStamp
+               mkfifo "${imgFile}" || exit 251
 
-                if [ "$(pv -V 2>/dev/null)" ]
-                        then
-                                {
-                                        dd if="${disk}" bs="${defaultBS}" | pv  --progress --wait --width 79 --size ${diskSize} | dd of="${imgFile}" bs="${defaultBS}"
-                                } 2>&1 &
-                        else
-                                dd if="${disk}" of="${imgFile}" bs="${defaultBS}" 2>&1 &
-                        fi
-                pidDD=$!
+               if [ "$(pv -V 2>/dev/null)" ]
+                       then
+                               {
+                                       dd if="${disk}" bs="${defaultBS}" | pv  --progress --wait --width 79 --size ${diskSize} | dd of="${imgFile}" bs="${defaultBS}"
+                               } 2>&1 &
+                       else
+                               dd if="${disk}" of="${imgFile}" bs="${defaultBS}" 2>&1 &
+                       fi
+               pidDD=$!
 
-                if ps -p ${pidDD} 2>&1 1>/dev/null
-                        then
-                                echo "[${TS}] ${CLI_param_action} process started."
-                                zip -9q --fifo "${imgFile}.zip" "${imgFile}" &
-                                pidZIP=$!
-                                while :
-                                        do
-                                                if ps -p ${pidZIP} 2>&1 1>/dev/null
-                                                        then
-                                                                if ! ps -p ${pidDD} 2>&1 1>/dev/null
-                                                                        then
-                                                                                if disown ${pidZIP} 2>&1 1>/dev/null; then kill -9 ${pidZIP} 2>&1 1>/dev/null; fi
-                                                                                getTimeStamp
-                                                                                echo "[${TS}] ${CLI_param_action} ERROR."
-                                                                                exit 250
-                                                                        fi
-                                                        else
-                                                                break
-                                                        fi
-                                                sleep 1
-                                        done
-                        else
-                                getTimeStamp
-                                echo "[${TS}] ${CLI_param_action} ERROR."
-                                exit 250
-                        fi
+               if ps -p ${pidDD} 2>&1 1>/dev/null
+                       then
+                               echo "[${TS}] ${CLI_param_action} process started."
+                               zip -q --fifo "${imgFile}.zip" "${imgFile}" &
+                               pidZIP=$!
+                               while :
+                                       do
+                                               if ps -p ${pidZIP} 2>&1 1>/dev/null
+                                                       then
+                                                               if ! ps -p ${pidDD} 2>&1 1>/dev/null
+                                                                       then
+                                                                               if disown ${pidZIP} 2>&1 1>/dev/null; then kill -9 ${pidZIP} 2>&1 1>/dev/null; fi
+                                                                               getTimeStamp
+                                                                               echo "[${TS}] ${CLI_param_action} ERROR."
+                                                                               exit 250
+                                                                       fi
+                                                       else
+                                                               break
+                                                       fi
+                                               sleep 1
+                                       done
+                       else
+                               getTimeStamp
+                               echo "[${TS}] ${CLI_param_action} ERROR."
+                               exit 250
+                       fi
 
-                        getTimeStamp
-                        echo -e "\n[${TS}] Testing ZIP archive."
-                        sync 2>/dev/null
-                        if ! zip -vT "${imgFile}.zip"; then exit 249; fi
-                        ;;
+                       getTimeStamp
+                       echo -e "\n[${TS}] Testing ZIP archive."
+                       sync 2>/dev/null
+                       if ! zip -vT "${imgFile}.zip"; then exit 249; fi
+                       ;;
 
-        "restore")
-                getTimeStamp
-                echo -e "\n[${TS}] Testing ZIP archive."
-                sync 2>/dev/null
-                if ! zip -vT "${imgFile}.zip"; then exit 249; fi
+       "restore")
+               getTimeStamp
+               echo -e "\n[${TS}] Testing ZIP archive."
+               sync 2>/dev/null
+               if ! zip -vT "${imgFile}.zip"; then exit 249; fi
 
-                imgSize=$(unzip -l "${imgFile}.zip" | tail -1 | grep -oE '^[0-9]+\s+1\s+file' | grep -oE '^[0-9]+')
-                readonly imgSize
-                if [ ! "$imgSize" ]
-                        then
-                                getTimeStamp
-                                echo "[${TS}] ERROR - Unsupported archive: ${imgFile}.zip"
-                                exit 248
-                        fi
+               imgSize=$(unzip -l "${imgFile}.zip" | tail -1 | grep -oE '^[0-9]+\s+1\s+file' | grep -oE '^[0-9]+')
+               readonly imgSize
+               if [ ! "$imgSize" ]
+                       then
+                               getTimeStamp
+                               echo "[${TS}] ERROR - Unsupported archive: ${imgFile}.zip"
+                               exit 248
+                       fi
 
-                echo
-                printf '%-30s %s\n'   "Backup image size:"     "${imgSize} bytes"
-                printf '%-30s %s\n'   "Disk size (${disk}):"   "${diskSize} bytes"
-                if [ "${imgSize}" -gt "${diskSize}" ]
-                        then
-                                getTimeStamp
-                                echo "[${TS}] ERROR - Disk size too small."
-                                exit 247
-                        fi
+               echo
+               printf '%-30s %s\n'   "Backup image size:"     "${imgSize} bytes"
+               printf '%-30s %s\n'   "Disk size (${disk}):"   "${diskSize} bytes"
+               if [ "${imgSize}" -gt "${diskSize}" ]
+                       then
+                               getTimeStamp
+                               echo "[${TS}] ERROR - Disk size too small."
+                               exit 247
+                       fi
 
-                getTimeStamp
-                echo "[${TS}] ${CLI_param_action} process started."
-                if [ "$(pv -V 2>/dev/null)" ]
-                        then
-                                if ! unzip -p "${imgFile}.zip" | pv  --progress --wait --width 79 --size ${imgSize} | dd of="${disk}" bs="${defaultBS}"
-                                        then
-                                                getTimeStamp
-                                                echo "[${TS}] ${CLI_param_action} ERROR."
-                                                exit 246
-                                        fi
-                        else
-                                if ! unzip -p "${imgFile}.zip" | dd of="${disk}" bs="${defaultBS}"
-                                        then
-                                                getTimeStamp
-                                                echo "[${TS}] ${CLI_param_action} ERROR."
-                                                exit 246
-                                        fi
-                                fi
-                        ;;
+               getTimeStamp
+               echo "[${TS}] ${CLI_param_action} process started."
+               if [ "$(pv -V 2>/dev/null)" ]
+                       then
+                               if ! unzip -p "${imgFile}.zip" | pv  --progress --wait --width 79 --size ${imgSize} | dd of="${disk}" bs="${defaultBS}"
+                                       then
+                                               getTimeStamp
+                                               echo "[${TS}] ${CLI_param_action} ERROR."
+                                               exit 246
+                                       fi
+                       else
+                               if ! unzip -p "${imgFile}.zip" | dd of="${disk}" bs="${defaultBS}"
+                                       then
+                                               getTimeStamp
+                                               echo "[${TS}] ${CLI_param_action} ERROR."
+                                               exit 246
+                                       fi
+                               fi
+                       ;;
 
-        *)
-                getTimeStamp
-                echo "[${TS}] ERROR - Invalid operation: ${CLI_param_action}"
-                exit 2
-                ;;
+       *)
+               getTimeStamp
+               echo "[${TS}] ERROR - Invalid operation: ${CLI_param_action}"
+               exit 2
+               ;;
 esac
 
 getTimeStamp
